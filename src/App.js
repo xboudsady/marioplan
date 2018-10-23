d303a4d23b2d7e2dc262e108ff598c66bfed4f82
Xayaseth Boudsady <boudsadyx@gmail.com>
2018-10-22 21:16:00 -0500

    Added React-Router-Dom and setup BrowserRtouer component wrapper

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Mario Plan</h1>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <h1>Mario Plan</h1>
+        </div>
+      </BrowserRouter>
     );
   }
 }
